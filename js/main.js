6eb119becd731012921b85350d149ea6cb1f3eb4
sheetal <sdharmaraj@qwinixtech.com>
2015-09-15 14:54:43 +0530

    Home page updated

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ jQuery(document).ready(function($){
 	$('.cd-main-nav').on('click', function(event){
 		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	});
+
+	//text for collapse button
+	$('.more').click(function(){
+		var $this = $(this);
+		$this.toggleClass('less');
+		if($this.hasClass('less')){
+			$this.html('Read Less');			
+		} else {
+			$this.html('Read More');
+		}
+	});
 });
+
+
